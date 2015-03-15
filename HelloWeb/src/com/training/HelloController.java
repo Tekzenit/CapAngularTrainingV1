7f9a28e5bbe27eb0ba6c62176e269ef1df1d19a9
Chris Hane <ChrisLHane@gmail.com>
2015-03-15 17:46:09 -0500

    Added fake APIs for customer and transaction

--- HelloWeb/src/com/training/HelloController.java
+++ HelloWeb/src/com/training/HelloController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloController{
  
-   @RequestMapping(value="/hello",method = RequestMethod.GET)
+   @RequestMapping(value = "/hello",method = RequestMethod.GET)
    public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello Spring MVC Framework!");
       return "hello";
    }
-
 }
